f9bf3bb2022a1eebd20c21555143b2dd9aaeecf9
uab-c1/310 <laurea987@gmail.com>
2018-09-20 15:33:51 +0200

    add "local" init files
    Now I can addd a list of files to be open in a local file
    in ~/ "home"

---
+++ .init-files.el
@@ -0,0 +1,6 @@
+~/Dropbox/Org/my.org
+;; ~/Dropbox/Math/Doctorat_Laura/crossBlowup/master.tex
+~/Dropbox/Org/Doc.org
+;; ~/Dropbox/Math/Pragmatics/Laura-TeX/finally-fainal-draft.tex
+~/Dropbox/bibliography/notes.org
+~/Dropbox/Org/ledger-2018.dat
